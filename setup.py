dea90dd25b282801f9423c4de533c44f1740cbce
Paulo Cagigal Aranguren <paulo@MacBook-Pro-de-Paulo.local>
2022-10-26 10:03:05 +0200

    Some changes

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'DataStats',
   packages = ['DataStats'],
-  version = '0.1',
+  version = '0.2',
   license='MIT',   
   description = 'This library consists of obtaining data from an open database "statsbombpy".',
   author = 'Iñigo Ugarte & Paulo Cagigal',
   author_email = 'inigo.ugarte@alumni.mondragon.edu',
   url = 'https://github.com/pauloo1010/Paulo-Inigo-Progra',
-  download_url = 'https://github.com/pauloo1010/Paulo-Inigo-Progra/archive/refs/tags/v_01.tar.gz',
+  download_url = 'https://github.com/pauloo1010/Paulo-Inigo-Progra/archive/refs/tags/v_02.tar.gz',
   keywords = ['Dataset', 'Easy-working', 'Open'],
   install_requires=[
           'pandas',
