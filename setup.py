033a34a1009a2a1b2e20efd8de53d732795a461f
Paulo Cagigal Aranguren <paulo@MacBook-Pro-de-Paulo.local>
2022-10-26 22:38:58 +0200

    Changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from pathlib import Path
 
-this_directory = Path(_file_).parent
+this_directory = Path(".").parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
